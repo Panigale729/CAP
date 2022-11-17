2ca63eba27a3f38060219ad897c555750d72a2fe
Rokas <Panigale729@gmail.com>
2022-11-18 00:26:51 +0200

    login and register back-end

--- cap/main/urls.py
+++ cap/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
      path("room/", views.room, name="room"),
-     path("login/", views.login, name="login"),
+     path("login/", views.loginPage, name="login"),
+     path("logout/", views.logoutUser, name="logout"),
      path("register/", views.register, name="register"),
  ]
--- cap/main/views.py
+++ cap/main/views.py
@@ -1,22 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm
 
 # Create your views here.
 
-def room(request):
-    return render(request, 'main/room.html')
 
-def login(request):
-    return render(request, 'main/login.html')
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('room')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('room')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        return render(request, 'main/login.html', {
+
+        })
 
 def register(request):
-    form = UserCreationForm()
+    if request.user.is_authenticated:
+        return redirect('room')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, f'Account was created for {user}')
 
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+                return redirect('login')
 
-    return render(request, 'main/register.html', {
-        'form': form,
-    })
+        return render(request, 'main/register.html', {
+            'form': form,
+        })
+    
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def room(request):
+    return render(request, 'main/room.html')
